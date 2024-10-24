fa2cd944493b5061820008b25bfa0e202bd5e1e8
kazgar <kazimierz.garstecki@gmail.com>
2024-10-25 00:14:21 +0200

    the way year directories are created need changing

--- src/main.py
+++ src/main.py
@@ -8,7 +8,9 @@ def main():
     check_for_dest()
     
     pattern_matches_to_dest_r(USER_ROOT_PATH, REGEX_PATTERN, STORING_DESTINATION)
-    create_directories_month_year(STORING_DESTINATION)
+    dates = create_year_dirs(STORING_DESTINATION)
+    create_month_dirs(STORING_DESTINATION, dates)
+
     
 
 def pattern_matches_to_dest_r(path, pattern, destination):
@@ -22,7 +24,7 @@ def pattern_matches_to_dest_r(path, pattern, destination):
             except:
                 pass
 
-def create_directories_month_year(path):
+def create_year_dirs(path):
     dates = {}
     pattern = re.compile(r"(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})")
     for file in os.listdir(path):
@@ -30,13 +32,22 @@ def create_directories_month_year(path):
         if date:
             year, month = date[0][2], date[0][1]
             if year not in dates:
-                dates[year] = [{month: path+file}]
+                dates[year] = [{month: f"{path}{year}"}]
             elif month not in dates[year]:
-                dates[year].append({month: path+file})
-
+                dates[year].append({month: f"{path}{year}"})
     for year in dates.keys():
-        if not os.path.isfile(path+year):
-            os.mkdir(path+year)
+        if not os.path.isdir(f"{path}{year}"):
+            os.mkdir(f"{path}{year}")
+    return dates
+
+def create_month_dirs(path, dates_dict):
+    for year in dates_dict:
+        for item in dates_dict[year]:
+            for month_path_dict in dates_dict[year][item]:
+                for month, file_path in month_path_dict:
+                    print(f"month {month}, file_path {file_path}")
+        
+            
         
             
                 
