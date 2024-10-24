72ce57d29b6180500d7062570ec201e34b492cbe
kazgar <kazimierz.garstecki@gmail.com>
2024-10-24 18:05:45 +0200

    added pattern_matches_to_dest_r() in main.py

--- src/SETUP.py
+++ src/SETUP.py
@@ -1,7 +1,9 @@
 import os
+import re
 
-USER_ROOT_PATH = "/Users/kazikgarstecki/"
-STORING_LOCATION = "/Users/kazikgarstecki/Desktop/FV_folder"
+USER_ROOT_PATH = "/Users/kazikgarstecki/Desktop/test_dir"
+STORING_DESTINATION = "/Users/kazikgarstecki/Desktop/FV_folder"
+REGEX_PATTERN = re.compile("FV_01-10-2022.jpg")
 
 def check_for_valid_paths():
     if not os.path.exists(USER_ROOT_PATH):
--- src/main.py
+++ src/main.py
@@ -1,23 +1,28 @@
 import os
 import shutil
+import re
 from SETUP import *
-
 
 def main():
     check_for_valid_paths()
     
-    '''
-    shutil.move("/Users/kazikgarstecki/Desktop/bilet.pdf", "/Users/kazikgarstecki/Desktop/moje_pliki/")
-    '''
-
-    if not os.path.isdir(STORING_LOCATION):
+    if not os.path.isdir(STORING_DESTINATION):
         try:
-            os.mkdir(STORING_LOCATION)
+            os.mkdir(STORING_DESTINATION)
         except:
-            raise ValueError("STORING_LOCATION is invalid")
+            raise ValueError("STORING_DESTINATION is invalid")
     
+    pattern_matches_to_dest_r(USER_ROOT_PATH, REGEX_PATTERN, STORING_DESTINATION)
     
 
+def pattern_matches_to_dest_r(path, pattern, destination):
+    if os.path.isdir(path):
+        for file in os.listdir(path):
+            pattern_matches_to_dest_r(path=f"{path}/{file}", pattern=pattern, destination=destination)    
+    elif os.path.isfile(path):
+        if pattern.match(os.path.split(path)[1]):
+            shutil.move(path, destination)
+    
     
 
 if __name__ == "__main__":
