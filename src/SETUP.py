6a40581baf311d9e21b89a388eb7937aefb7a03a
kazgar <kazimierz.garstecki@gmail.com>
2024-10-24 23:34:22 +0200

    added creating directories in set destination

--- src/SETUP.py
+++ src/SETUP.py
@@ -3,7 +3,7 @@ import re
 
 USER_ROOT_PATH = "/Users/kazikgarstecki/Desktop/test_dir"
 STORING_DESTINATION = "/Users/kazikgarstecki/Desktop/FV_folder/"
-REGEX_PATTERN = re.compile(r"^FV_(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})\.(pdf|docx)$")
+REGEX_PATTERN = re.compile(r"^FV_.*(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})\.(pdf|docx)$")
 
 def check_for_valid_paths():
     if not os.path.exists(USER_ROOT_PATH):
--- src/main.py
+++ src/main.py
@@ -9,8 +9,7 @@ def main():
     
     pattern_matches_to_dest_r(USER_ROOT_PATH, REGEX_PATTERN, STORING_DESTINATION)
 
-
-    
+    create_directories_month_year(STORING_DESTINATION)
 
 def pattern_matches_to_dest_r(path, pattern, destination):
     if os.path.isdir(path):
@@ -24,7 +23,22 @@ def pattern_matches_to_dest_r(path, pattern, destination):
                 pass
 
 def create_directories_month_year(path):
-    return
+    dates = {}
+    pattern = re.compile(r"(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})")
+    for file in os.listdir(path):
+        date = pattern.findall(file)
+        if date:
+            year, month = date[0][2], date[0][1]
+            if year not in dates:
+                dates[year] = [{month: path+file}]
+            elif month not in dates[year]:
+                dates[year].append({month: path+file})
+    for year in dates.keys():
+        os.mkdir(path+year)
+
+                
+                    
+        
 
     
 
