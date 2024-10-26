5797d311f426703a29854deb6c7610d8a33dfd4a
kazgar <kazimierz.garstecki@gmail.com>
2024-10-26 18:16:45 +0200

    changed SETUP.py to be universal

--- src/SETUP.py
+++ src/SETUP.py
@@ -1,8 +1,8 @@
 import os
 import re
 
-USER_ROOT_PATH = "/Users/kazikgarstecki/Desktop"
-STORING_DESTINATION = "/Users/kazikgarstecki/Desktop/FV/"
+USER_ROOT_PATH = "/path/to/destination/to/look"
+STORING_DESTINATION = "path/to/storing/destination/name_of_the_folder_to_store_files_in"
 REGEX_PATTERN = re.compile(r"^(FV_|fv_).*(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})\.(pdf|docx)$")
 DRIVE_ROOT_FOLDER_NAME = "FV"
 
